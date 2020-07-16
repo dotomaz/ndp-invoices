a37fcee65e61c1c5f2dadd788558030a0f747c54
Tomaž Dobrišek <tomaz.dobrisek@dlabs.si>
2020-07-17 00:00:56 +0200

    fix the protocol

--- src/services/Host.ts
+++ src/services/Host.ts
@@ -2,6 +2,6 @@ export function getHost(){
     if( window.location.hostname === "localhost" ) {
         return `http://localhost`;
     } else {
-        return `${window.location.protocol}://${window.location.host}`
+        return `${window.location.protocol}//${window.location.host}`
     }
 }
