002c58eaa34cb7c25b25f6449253728752e2acac
Tomaž Dobrišek <tomaz.dobrisek@dlabs.si>
2020-09-05 16:19:20 +0200

    debug 2

--- laravel-app/app/Lib/Importer.php
+++ laravel-app/app/Lib/Importer.php
@@ -47,7 +47,7 @@ class Importer {
                         echo "<br>Napaka: <b>". $ex->getMessage() ."</b><br>";
                     }
 
-                    $price = intval($this->arr($row, 16));
+                    $price = intval(preg_replace( '#[^0-9\.]#gi', '', str_replace(',', '.', $this->arr($row, 16))));
                     dump([$childName, $this->arr($row, 16), $price]);
                     if( $price > 0 ) {
                         if ( is_null($invoice) ) {
